e93858ea73eb66e8755651a9a73c307e34323874
leah-hirsch <leahmhirsch@gmail.com>
2024-11-08 15:01:55 -0500

    Delete prob3.py

--- prob3.py
+++
@@ -1,4 +0,0 @@
-import pandas as pd
-url = "https://archive.ics.uci.edu/ml/machine-learning-databases/iris/iris.data"
-column_names = ["sepal_length", "sepal_width", "petal_length", "petal_width", "species"]
-iris_data = pd.read_csv(url, header=None, names=column_names)

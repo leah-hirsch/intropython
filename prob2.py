d2ac706da57390a740fd0a42892d92c6fb42e2d1
yyoshiaki <y.yasumizu@gmail.com>
2024-11-08 13:11:40 -0500

    fix error

--- prob2.py
+++ prob2.py
@@ -7,14 +7,12 @@
 #question 2: read csv and calculate the stats (mean, median, std)
 import pandas as pd
 
+url = "https://archive.ics.uci.edu/ml/machine-learning-databases/iris/iris.data"
+
 # Load the data
-df_iris = pd.read_csv(url, header=None, names=column_names)
 column_names = ["sepal_length", "sepal_width", "petal_length", "petal_width", "species"]
+df_iris = pd.read_csv(url, header=None, names=column_names)
 
 # Calculate statistics for sepal and petal dimensions'
 sepal_petal_stats = df_iris[["sepal_length", "sepal_width", "petal_length", "petal_width"]].agg(["mean", "median", "std"])
 print(sepal_petal_stats)
-
-
-
-

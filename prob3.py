dc930e4e2e50aa704c6ed1ff549a2e1fdec97d07
leah-hirsch <leahmhirsch@gmail.com>
2024-11-08 14:59:56 -0500

    png

---
+++ prob3.py
@@ -0,0 +1,4 @@
+import pandas as pd
+url = "https://archive.ics.uci.edu/ml/machine-learning-databases/iris/iris.data"
+column_names = ["sepal_length", "sepal_width", "petal_length", "petal_width", "species"]
+iris_data = pd.read_csv(url, header=None, names=column_names)
